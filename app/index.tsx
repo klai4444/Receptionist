dde10d0c34a785c8e2279bfca7fc762b6c50c57e
JulianTang2027 <juliantang2027@u.northwestern.edu>
2025-01-28 16:45:25 -0600

    Awesome front end

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Receptionist</Text>
+      <Text style={styles.body}>Virtual receptionist, anytime, anywhere, anyone.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'RobotoSlab-Bold', // Match the file name
+    fontSize: 24,
+  },
+  body: {
+    fontFamily: 'RobotoSlab-Medium', // Match the file name
+    fontSize: 18,
+  },
+});
+
