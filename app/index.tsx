874a7aed73d6a2c2002d3b9fa620f36e02aa6d84
Kellen Lai <klai8899@gmail.com>
2025-01-28 16:50:37 -0600

    bug

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, View, StyleSheet } from "react-native";
-import { Text, View } from "react-native";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
